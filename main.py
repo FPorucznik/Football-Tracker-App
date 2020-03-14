4e6baa9ab6e4190454e561da2c258f0786fb22ae
fporucznik <porucznik199@wp.pl>
2020-03-14 16:45:51 +0100

    window

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import tkinter
-'''Football Tracker alpha'''
+import tkinter as tk
+'''Football Tracker alpha version'''
+
+window = tk.Tk()
+window.title("Football Tracker")
+label = tk.Label(window, text = "Menu powitalne aplikacji Football Tracker")
+label.pack(side = tk.BOTTOM)
+tk.mainLoop()
