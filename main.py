913aca095e5e1dda438048bf47352243a68858fc
FPorucznik <porucznik199@wp.pl>
2020-03-14 17:48:40 +0100

    window update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.font import Font
 #Football Tracker alpha version
 
 #wygenerowanie okna
@@ -8,10 +9,14 @@ window.title("Football Tracker")
 window.iconbitmap("assets/football.ico")
 
 #powitalny tekst
-welcome = tk.Label(text = "Witaj w aplikacji Football Tracker")
-welcome.pack()
+welcome = tk.Label(text = "Witaj w aplikacji Football Tracker", font="Arial")
+welcome.pack(pady=30)
 
-buttonTables = tk.Button(text = "Tabele", width = 20, height = 5)
-buttonTables.pack()
+buttonTables = tk.Button(text = "Tabele", width = 20, height = 2, font="Arial")
+buttonTables.pack(pady=20)
+buttonGoalScorers = tk.Button(text = "Klasyfikacja strzelców", width = 20, height = 2, font="Arial")
+buttonGoalScorers.pack(pady=20)
+buttonMatches = tk.Button(text = "Dzisiejsze mecze", width = 20, height = 2, font="Arial")
+buttonMatches.pack(pady=20)
 
 window.mainloop()
