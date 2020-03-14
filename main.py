f359c443614380ef296372dd10c994a9820ba03e
FPorucznik <porucznik199@wp.pl>
2020-03-14 17:09:32 +0100

    window commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
-'''Football Tracker alpha version'''
+#Football Tracker alpha version
 
+#wygenerowanie okna
 window = tk.Tk()
+window.geometry("500x500+400+100")
 window.title("Football Tracker")
-label = tk.Label(window, text = "Menu powitalne aplikacji Football Tracker")
-label.pack(side = tk.BOTTOM)
-tk.mainLoop()
+window.iconbitmap("assets/football.ico")
+
+#powitalny tekst
+welcome = tk.Label(text = "Witaj w aplikacji Football Tracker")
+welcome.pack()
+
+buttonTables = tk.Button(text = "Tabele", width = 20, height = 5)
+buttonTables.pack()
+
+window.mainloop()
