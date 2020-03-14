8cde81af5eea24186d076f7aeedffe6869800b0c
FPorucznik <porucznik199@wp.pl>
2020-03-14 20:34:43 +0100

    menu update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tkinter as tk
-from tkinter.font import Font
 #Football Tracker alpha version
+import tkinter as tk
 
 #wygenerowanie okna
 window = tk.Tk()
@@ -8,15 +7,63 @@ window.geometry("500x500+400+100")
 window.title("Football Tracker")
 window.iconbitmap("assets/football.ico")
 
-#powitalny tekst
-welcome = tk.Label(text = "Witaj w aplikacji Football Tracker", font="Arial")
-welcome.pack(pady=30)
+#funkcja czyszcząca okno, wykorzystuje ją przy wyborze opcji z menu
+def clearWindow():
+    children = window.winfo_children()
+    for child in children:
+        child.destroy()
 
-buttonTables = tk.Button(text = "Tabele", width = 20, height = 2, font="Arial")
-buttonTables.pack(pady=20)
-buttonGoalScorers = tk.Button(text = "Klasyfikacja strzelców", width = 20, height = 2, font="Arial")
-buttonGoalScorers.pack(pady=20)
-buttonMatches = tk.Button(text = "Dzisiejsze mecze", width = 20, height = 2, font="Arial")
-buttonMatches.pack(pady=20)
+#menu startowe
+def welcomeMenu():
+    clearWindow()
+    welcome = tk.Label(text = "Witaj w aplikacji Football Tracker", font="Arial")
+    welcome.pack(pady=30)
 
+    buttonTables = tk.Button(text = "Tabele", width = 20, height = 2, font="Arial", command=tables)
+    buttonTables.pack(pady=20)
+    buttonGoalScorers = tk.Button(text = "Klasyfikacja strzelców", width = 20, height = 2, font="Arial", command=goalScorers)
+    buttonGoalScorers.pack(pady=20)
+    buttonMatches = tk.Button(text = "Dzisiejsze mecze", width = 20, height = 2, font="Arial", command=matches)
+    buttonMatches.pack(pady=20)
+
+#sekcja tabel
+def tables():
+    clearWindow()
+    title = tk.Label(text = "Wybierz Ligę", font="Arial")
+    title.pack()
+
+    buttonReturn = tk.Button(text = "Wróć do menu", width = 20, height = 1, font="Arial", command=welcomeMenu)
+    buttonReturn.pack(pady=20)
+
+    buttonPremierLeague = tk.Button(text = "Premier League", width = 20, height = 1, font="Arial")
+    buttonPremierLeague.pack(pady=10)
+
+    buttonLaLiga = tk.Button(text = "LaLiga", width = 20, height = 1, font="Arial")
+    buttonLaLiga.pack(pady=10)
+
+    buttonBundesliga = tk.Button(text = "Bundesliga", width = 20, height = 1, font="Arial")
+    buttonBundesliga.pack(pady=10)
+
+    buttonSerieA = tk.Button(text = "Serie A", width = 20, height = 1, font="Arial")
+    buttonSerieA.pack(pady=10)
+
+#sekcja strzelców
+def goalScorers():
+    clearWindow()
+    title = tk.Label(text = "Klasyfikacja strzelców", font="Arial")
+    title.pack()
+
+    buttonReturn = tk.Button(text = "Wróć do menu", width = 20, height = 1, font="Arial", command=welcomeMenu)
+    buttonReturn.pack(pady=20)
+
+#sekcja meczów
+def matches():
+    clearWindow()
+    title = tk.Label(text = "Dzisiejsze mecze", font="Arial")
+    title.pack()
+
+    buttonReturn = tk.Button(text = "Wróć do menu", width = 20, height = 1, font="Arial", command=welcomeMenu)
+    buttonReturn.pack(pady=20)
+
+welcomeMenu()
 window.mainloop()
